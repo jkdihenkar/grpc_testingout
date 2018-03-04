488cb01d01f48c917d914103a94133c678c10839
jkdihenkar <jkdihenkar@gmail.com>
2018-03-04 12:34:51 +0530

    Introducing rx in clients

--- echo_client.py
+++ echo_client.py
@@ -1,9 +1,10 @@
 import grpc
-from concurrent import futures
 
+import rx
 import helloworld_pb2
 import helloworld_pb2_grpc
 
+import logging
 from cython import nogil
 
 channels = [ 
@@ -13,20 +14,22 @@ stubs = [
     helloworld_pb2_grpc.GreeterStub(channels[0])
 ]
 
+logging.getLogger().setLevel(logging.INFO)
+
+pooled_scheduler = rx.concurrency.ThreadPoolScheduler(max_workers=80)
+
 
 def run():
-    p = futures.ThreadPoolExecutor(max_workers=500)
-    data_set = range(10000, 0, -1)
-    p.map(make_req_and_get_response, data_set)
+    rx.Observable.from_(range(10000, 0, -1)) \
+        .subscribe_on(pooled_scheduler) \
+        .subscribe(on_next=lambda x: make_req_and_get_response(x))
 
 
 def make_req_and_get_response(i):
     response = stubs[0].sayHello(helloworld_pb2.HelloRequest(
             name="JayD + Req {}".format(i)
         ))
-    print("Greeter client received :: {}".format(
-            response.message
-        ))
+    logging.info("Greeter client received :: %s", response.message)
 
 
 if __name__ == "__main__":
--- echo_server.py
+++ echo_server.py
@@ -7,22 +7,24 @@ from cython import nogil
 import helloworld_pb2
 import helloworld_pb2_grpc
 import logging
+import hashlib
+
+executor = futures.ThreadPoolExecutor(max_workers=50)
 
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
     def sayHello(self, request, context):
         return helloworld_pb2.HelloReply(
-            message="Hello world .... {}".format(
-                request.name
+            message="Hello world .... {} ( Integrity: {})".format(
+                request.name,
+                hashlib.sha512(request.name.encode('utf-8')).hexdigest()
             )
         )
 
 
 def app():
-    server = grpc.server(
-        futures.ThreadPoolExecutor(max_workers=50)
-    )
+    server = grpc.server(executor)
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port('localhost:27885')
     server.start()
@@ -30,7 +32,7 @@ def app():
         while True:
             time.sleep(1)
     except Exception as e:
-        server.stop()
+        server.stop(1)
 
 
 if __name__ == "__main__":
